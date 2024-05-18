782bc912c82905bffe1139c4ce661d2313214103
Andy <161436408+Andyl24061@users.noreply.github.com>
2024-05-18 19:58:07 +1200

    Create Multiplication.py

---
+++ Multiplication.py
@@ -0,0 +1,19 @@
+positiveinteger = False
+while not positiveinteger:
+    num = int(input("Enter a possitive integer"))
+    if num > 0:
+        positiveinteger = True
+        print(num * 1)
+        print(num * 2)
+        print(num * 3)
+        print(num * 4)
+        print(num * 5)
+        print(num * 6)
+        print(num * 7)
+        print(num * 8)
+        print(num * 9)
+        print(num * 10)
+        print(num * 11)
+        print(num * 12)
+    else:
+        print("That is not a positive integer")
